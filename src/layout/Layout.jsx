52d2adcdfc449b7e837ebc844eb262c189b94a22
hossein <hosseinbakhtiari781@outlook.com>
2024-05-11 09:14:04 +0330

    implement final layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import styles from "./layout.module.css";
+import { useSelector } from "react-redux";
 // import { useCart } from "../context/CartContext";
 function Layout({ children }) {
- // const [state] = useCart();
+  const state = useSelector((store) => store.cart);
   return (
-    <> 
+    <>
       <header className={styles.header}>
         <Link to="/products">Shopping App</Link>
         <Link to="/checkout">
           <div>
             <PiShoppingCartSimpleBold />
-            {/* {!!state.itemsCounter && <span>{state.itemsCounter}</span>} */}
+            {!!state.itemsCounter && <span>{state.itemsCounter}</span>}
           </div>
         </Link>
       </header>
